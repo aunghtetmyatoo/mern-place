659d9d9799a0d634456e502f99724e39463b6c5e
ahmo <aunghtetmyatoo@ucsy.edu.mm>
2023-09-12 10:10:19 +0630

    crud place

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const placeRoutes = require("./routes/place-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/places", placeRoutes);
+
+//route error handling
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route!", 404);
+});
 
 app.use((err, req, res, next) => {
   if (err.headersSent) {
--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,12 +1,15 @@
 const HttpError = require("../models/http-error");
+const { v4: uuid } = require("uuid");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Dagon Center 3, Yangon",
+    description: "One of the most famous shopping center in Yangon",
+    address: "No. 1, Pyay Road, Dagon Township, Yangon, Myanmar (Burma)",
     location: {
       lat: 16.8043886,
-      long: 96.1377029,
+      lng: 96.1377029,
     },
     creator: "u1",
   },
@@ -15,7 +18,7 @@ const DUMMY_PLACES = [
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => {
-    return p.id == placeId;
+    return p.id === placeId;
   });
 
   if (!place) {
@@ -25,18 +28,66 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator == userId;
+  const places = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(new HttpError("Could not find a place!", 404));
   }
 
-  res.json({ place });
+  res.json({ places });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+
+  const placeId = req.params.pid;
+  const updatedPlace = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  const placeIndex = DUMMY_PLACES.findIndex((p) => {
+    return p.id === placeId;
+  });
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json(updatedPlace);
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => {
+    return p.id !== placeId;
+  });
+
+  res.status(200).json({ message: "Deleted place." });
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
